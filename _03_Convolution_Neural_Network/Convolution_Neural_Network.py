4cc7f1570b489cf8add6cf00dad095d6be55e293
= <=>
2023-06-23 15:01:45 +0800

    第五次提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -45,7 +45,7 @@ class ResidualBlock(nn.Module):
         return out
 
 
-# ResNet定义 
+# ResNet定义 网络结构
 class NeuralNetwork(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super(NeuralNetwork, self).__init__()
